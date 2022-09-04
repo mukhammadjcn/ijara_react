188cfaa9ccf7923e2af0e766a4452bad7e21f78a
mukhammadjcn <mukhammadjcn@gmail.com>
2022-09-04 21:59:46 +0500

    fixed one item

--- src/components/meta/index.tsx
+++ src/components/meta/index.tsx
@@ -11,7 +11,11 @@ const MetaDecorator = ({ title, description, imageUrl, imageAlt }: any) => (
     <meta property="og:image" content={imageUrl} />
     <meta
       property="og:url"
-      content={window.location.pathname + window.location.search}
+      content={
+        "https://ijara-react.vercel.app" +
+        window.location.pathname +
+        window.location.search
+      }
     />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image:alt" content={imageAlt} />
